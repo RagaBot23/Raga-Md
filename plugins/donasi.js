39b566aae71679554bcf13b1477c332462369821
RagaBot <93459204+RagaBot23@users.noreply.github.com>
2022-06-27 21:12:00 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,7 +1,7 @@
 let handler = async m => m.reply(`
-╭─「 Donasi • Pulsa 」
-│ •  [0895-3303-79186]
-│ •  [0896-5436-0447]
+╭─「 Donasi • Pulsa - Dana 」
+│ •  [089696345686]
+│ •  [089696345686]
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
